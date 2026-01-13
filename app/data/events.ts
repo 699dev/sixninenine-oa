3530149f72d94d6d870dce4112a32a8eecae1b71
lonewolfyx <877085893@qq.com>
2026-01-13 22:56:51 +0800

    refactor(events): Refactor the event data structure to a factory function pattern

--- app/data/events.ts
+++ app/data/events.ts
@@ -1,30 +1,23 @@
-import type { IEventDataSourceInput } from '#shared/types/events'
+import type { EventsType, IEventDataSourceInput } from '#shared/types/events'
+
+const createEvent = (
+    title: string,
+    date: string,
+    description: string = '正常直播',
+    type: EventsType = 'working',
+): IEventDataSourceInput => {
+    return {
+        title: title,
+        start: date,
+        allDay: true,
+        extendedProps: {
+            type: type,
+            description: description,
+        },
+    }
+}
 
 export const eventsData: IEventDataSourceInput[] = [
-    {
-        title: '请假',
-        start: '2026-01-12',
-        allDay: true,
-        extendedProps: {
-            type: 'skip',
-            description: '拉肚子',
-        },
-    },
-    {
-        title: '直播',
-        start: '2026-01-12',
-        allDay: true,
-        extendedProps: {
-            type: 'working',
-            description: '正常直播',
-        },
-    },
-    // {
-    //     title: '项目截止日期',
-    //     start: '2026-01-20',
-    //     allDay: true,
-    //     extendedProps: {
-    //         description: '项目提交截止',
-    //     },
-    // },
+    createEvent('请假', '2026-01-12', '拉肚子', 'skip'),
+    createEvent('直播', '2026-01-13'),
 ]
